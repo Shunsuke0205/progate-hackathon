95e4b15626b58481e37e74f4eac24c34eb524188
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-05 11:46:50 +0900

    impl: life

--- Assets/Game/OpponentCollision.cs
+++ Assets/Game/OpponentCollision.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class OpponentCollision : MonoBehaviour
 {
+    [SerializeField] private int playerLife = 3;
+    [SerializeField] private GameObject lifePrefab;
+
+    GameObject life1;
+    GameObject life2;
+    GameObject life3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        life1 = Instantiate(lifePrefab, new Vector3(0, 5, 0), Quaternion.identity);
+        life2 = Instantiate(lifePrefab, new Vector3(2, 5, 2), Quaternion.identity);
+        life3 = Instantiate(lifePrefab, new Vector3(4, 5, 4), Quaternion.identity);
     }
 
     int x_position_state = 1;
@@ -23,6 +32,15 @@ public class OpponentCollision : MonoBehaviour
         Debug.Log(oponent.gameObject.name);
         if (oponent.gameObject.name == "CubeObstacle") {
             Destroy(oponent.gameObject);
+            playerLife -= 1;
+            if (playerLife == 2) {
+                Destroy(life1);
+            } else if (playerLife == 1) {
+                Destroy(life2);
+            } else if (playerLife == 0) {
+                Destroy(life3);
+                // TODO: Game Over
+            }
         }
         
     }
