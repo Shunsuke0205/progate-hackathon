50cd10282238c5d480c62170ccd8a8ffc268033e
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-05 12:04:19 +0900

    impl: variable life num

--- Assets/Game/OpponentCollision.cs
+++ Assets/Game/OpponentCollision.cs
@@ -7,16 +7,14 @@ public class OpponentCollision : MonoBehaviour
     [SerializeField] private int playerLife = 3;
     [SerializeField] private GameObject lifePrefab;
 
-    GameObject life1;
-    GameObject life2;
-    GameObject life3;
+    List<GameObject> lifeList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        life1 = Instantiate(lifePrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        life2 = Instantiate(lifePrefab, new Vector3(2, 5, 2), Quaternion.identity);
-        life3 = Instantiate(lifePrefab, new Vector3(4, 5, 4), Quaternion.identity);
+        for (int i = 0; i < playerLife; i++) {
+            lifeList.Add(Instantiate(lifePrefab, new Vector3(i * 2, 5, i * 2), Quaternion.identity));
+        }
     }
 
     int x_position_state = 1;
@@ -33,13 +31,11 @@ public class OpponentCollision : MonoBehaviour
         if (oponent.gameObject.name == "CubeObstacle") {
             Destroy(oponent.gameObject);
             playerLife -= 1;
-            if (playerLife == 2) {
-                Destroy(life1);
-            } else if (playerLife == 1) {
-                Destroy(life2);
-            } else if (playerLife == 0) {
-                Destroy(life3);
-                // TODO: Game Over
+            if (playerLife >= 0) {
+                Destroy(lifeList[playerLife]);
+            }
+            if (playerLife == 0) {
+                Debug.Log("Game Over");
             }
         }
         
